bdf1056c8e3022d40a4e3108ac59954d2732e686
sithulwin-mhs <sithulwin.mhs@gmail.com>
2021-09-19 00:05:27 +0630

    my portfolio

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = (props) => (
+  <>
+    <Section row nopadding>
+      <LeftSection>
+        <SectionTitle main center>
+          Welcome To <br />
+          My Personal Portfolio
+        </SectionTitle>
+        <SectionText>
+        I work as a junior web developer and good experience with react js. I also have a fair experience in the backend. I am eager to learn new technologies.
+        </SectionText>
+        {/* <Button onClick={props.handleClick}>Learn More</Button> */}
+      </LeftSection>
+    </Section>
+  </>
+);
+
+export default Hero;
